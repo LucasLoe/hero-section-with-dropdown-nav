a9b1900ab682ea7455e042d1a364c274a1327a57
Lucas L <lucas_loe@gmx.de>
2022-09-25 20:25:06 +0200

    project superficially finished

--- stuff.js
+++ stuff.js
@@ -1,5 +1,35 @@
-console.log('loaded')
-
 function menuToggle() {
-    console.log('toggle me, daddy')
+    let navMobileMenu = document.getElementById('nav-mobile-menu');
+    let navOverlay = document.getElementById('nav-mobile-overlay')
+
+    navMobileMenu.classList.toggle('invisible');
+    navOverlay.classList.toggle('invisible');
+}
+
+function openFeaturesSub() {
+    let submenu = document.getElementById('nav-hover-features');
+    submenu.classList.toggle('invisible');
+}
+
+function openFeaturesSubMobile() {
+    let submenu = document.getElementById('nav-hover-features-mobile');
+    submenu.classList.toggle('invisible');
+}
+
+function openCompanySub() {
+    let submenu = document.getElementById('nav-hover-company');
+    submenu.classList.toggle('invisible');
+}
+
+function openCompanySubMobile() {
+    let submenu = document.getElementById('nav-hover-company-mobile');
+    submenu.classList.toggle('invisible');
+}
+
+function closeMenu() {
+    let navMobileMenu = document.getElementById('nav-mobile-menu');
+    let navOverlay = document.getElementById('nav-mobile-overlay')
+
+    navMobileMenu.classList.add('invisible');
+    navOverlay.classList.add('invisible');
 }
